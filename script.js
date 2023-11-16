6ab532fb44e4fc13c23773931ed32c9a6fccb180
RJ Ocampo <rjqocampo@gmail.com>
2023-11-17 05:31:15 +0800

    Add delete feature

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function createTree(arr) {
     }
   };
 
-  function insert(input, node = root) {
+  function insertNode(input, node = root) {
     console.log(node);
     if (node === null) {
       node = createNode(input);
@@ -95,22 +95,65 @@ function createTree(arr) {
     }
 
     if (input < node.data) {
-      node.left = insert(input, node.left);
+      node.left = insertNode(input, node.left);
     } else if (input > node.data) {
-      node.right = insert(input, node.right);
+      node.right = insertNode(input, node.right);
     }
 
     return node;
   }
 
-  return { prettyPrint, insert };
+  function deleteNode(input, node = root) {
+    if (node === null) {
+      return node;
+    }
+
+    if (input < node.data) {
+      node.left = deleteNode(input, node.left);
+      return node;
+    } else if (input > node.data) {
+      node.right = deleteNode(input, node.right);
+      return node;
+    }
+
+    if (node.left === null) {
+      let temp = node.right;
+      delete node;
+      return temp;
+    } else if (node.right === null) {
+      let temp = node.left;
+      delete node;
+      return temp;
+    } else {
+      let succParent = node;
+      let succ = node.right;
+
+      while (succ.left !== null) {
+        succParent = succ;
+        succ = succ.left;
+      }
+
+      if (succParent !== node) {
+        succParent.left = succ.right;
+      } else {
+        succParent.right = succ.right;
+      }
+
+      node.data = succ.data;
+
+      delete succ;
+      return node;
+    }
+  }
+
+  return { prettyPrint, insertNode, deleteNode };
 }
 
-const orderedTestArray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+const orderedTestArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const randomTestArray = createRandomArray();
 
 const bst = createTree(orderedTestArray);
-bst.insert(1);
+bst.deleteNode(5);
 bst.prettyPrint();
 
 /* 
